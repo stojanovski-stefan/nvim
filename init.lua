59574021678873664221fe29abf9fc0b377130bc
Stefan <105825004+sstoj1@users.noreply.github.com>
2025-11-30 14:08:05 -0500

    Add alternative comment shortcut
    Useful when using tmux, "ctrl + /" might not work when using tmux

--- init.lua
+++ init.lua
@@ -30,6 +30,9 @@ vim.g.maplocalleader = "\\"
 vim.keymap.set('n', '<C-/>', 'gcc', { remap = true })
 vim.keymap.set('v', '<C-/>', 'gc', { remap = true })
 
+vim.keymap.set('n', '<C-_>', 'gcc', { remap = true })
+vim.keymap.set('v', '<C-_>', 'gc', { remap = true })
+
 -- Setup lazy.nvim
 require("lazy").setup("plugins")
 
